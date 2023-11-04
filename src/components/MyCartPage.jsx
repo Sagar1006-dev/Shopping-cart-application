8b1477f73ea3999c84b025801f2882d0975dee97
Sagar Muneshwar <129708548+Sagar1006-dev@users.noreply.github.com>
2023-11-04 22:04:11 +0530

    seond

--- src/components/MyCartPage.jsx
+++ src/components/MyCartPage.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+// CartPage.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const MyCartPage = () => {
-  return (
-    <div>MyCartPage</div>
-  )
-}
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-export default MyCartPage
+  const handleRemoveFromCart = (productId) => {
+    dispatch(removeFromCart(productId));
+  };
+
+  const handleCheckout = () => {
+    // Dispatch an action to reset the cart and show a success message
+    dispatch(checkout());
+    alert('Items have been checked out successfully!');
+  };
+
+  return (
+    <div>
+      <h1>My Cart</h1>
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>
+            <p>{item.title}</p>
+            <p>Price: {item.price}</p>
+            <button onClick={() => handleRemoveFromCart(item.id)}>
+              Remove from Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="checkout-sidebar">
+        <h2>Checkout</h2>
+        <ul>
+          {cart.map((item) => (
+            <li key={item.id}>
+              <p>{item.title}</p>
+              <p>Price: {item.price}</p>
+            </li>
+          ))}
+        </ul>
+        <p>Total Price: ${cart.reduce((total, item) => total + item.price, 0)}</p>
+        <button onClick={handleCheckout}>Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default MyCartPage;
